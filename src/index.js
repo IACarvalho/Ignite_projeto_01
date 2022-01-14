492d968a4bff304ceb3a11b966376ebb0703acf4
israel.neto <israel.neto@odontosystem.com.br>
2022-01-14 09:55:50 -0300

    Created withdraw route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { application, request } = require('express');
+const { application, request, response } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -23,6 +23,17 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
 // Creatae account
 app.post("/account", (request, response) => {
   const {cpf, name} = request.body;
@@ -67,5 +78,27 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
 
   return response.status(201).send();
 })
+
+// Debit
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer  } = request;
+  const { amount } = request.body;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({"error": "Insufficient founds"});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
 
 app.listen(3333, () => console.log('Server running at port 3333'));
